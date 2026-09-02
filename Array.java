32db60d1ebb9fddf43bfc79e04bd804936303b77
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-09-02 22:03:23 +0530

    12

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
