468e41bd15736aecff09880d5293831dd9904ea9
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-09-02 21:56:09 +0530

    3

---
+++ variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String[] args) {
+        int age = 20;
+        float height = 5.8f;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
