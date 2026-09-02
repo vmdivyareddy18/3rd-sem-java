b0fb6177f72546503f07faf9d000b17e67b3e77e
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-09-02 21:57:02 +0530

    4

---
+++ UserInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+        sc.close();
+    }
+}
