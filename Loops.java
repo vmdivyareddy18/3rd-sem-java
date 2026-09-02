9e60de6e38904940363dc6d4e32eb3d8c3cb1a73
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-09-02 22:01:53 +0530

    10

---
+++ Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        System.out.println("Numbers from 1 to 10:");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
