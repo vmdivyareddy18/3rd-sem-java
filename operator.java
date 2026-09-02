3f52794757c7b9b0ac24e208dd9b0539d37e8976
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-09-02 21:58:12 +0530

    5

---
+++ operator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class operator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Modulus = " + (a % b));
+
+        sc.close();
+    }
+}
