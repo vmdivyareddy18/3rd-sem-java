b775818810a15ff4c60b51907574d85497c0c73e
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-09-02 22:02:39 +0530

    11

---
+++ switch.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Addition = " + (a + b));
+                break;
+            case 2:
+                System.out.println("Subtraction = " + (a - b));
+                break;
+            case 3:
+                System.out.println("Multiplication = " + (a * b));
+                break;
+            case 4:
+                if (b != 0)
+                    System.out.println("Division = " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero.");
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+
+        sc.close();
+    }
+}
