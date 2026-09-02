5d7eebb403a99acdf266a57e76f6f0884380fc15
V.M Divya Reddy <vmdivyareddy18@gmail.com>
2026-09-02 22:04:44 +0530

    14

---
+++ Homework.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of first " + n + " natural numbers = " + sum);
+
+        sc.close();
+    }
+}
